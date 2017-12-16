a63db80d10fc38ebdeb044c911ee6406b453bf71
Gustavo Bueno <gustavo.bueno89@gmail.com>
2017-12-16 17:44:38 +0100

    can navigate from header to create note

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 
 import { NotesStyle } from '../styles/index';
 import { notes } from '../selectors/notes';
-import EmptyNotes from '../components/Notes/EmptyNotes';
 import { goto } from '../actions/navigation';
+import EmptyNotes from '../components/Notes/EmptyNotes';
+import CreateNoteButton from '../components/common/CreateNoteButton';
 
 class Notes extends Component {
+    static navigationOptions = ({ navigation }) => {
+        const { state } = navigation;
+        const gotoCreateNote = state.params && state.params.gotoCreateNote ? state.params.gotoCreateNote : null;
+        return {
+            title: 'Notes',
+            headerRight: <CreateNoteButton gotoCreateNote={gotoCreateNote} />
+        }
+    };
 
-    static navigationOptions = ({ navigation }) => ({
-        title: 'Notes',
-    });
+    componentDidMount() {
+        this.props.navigation.setParams({
+            gotoCreateNote: this.props.gotoCreateNote
+        });
+    }
 
     renderNotes() {
         console.log(this.props.notes);
